fedfa6d3aac96d60bd4c69acc8edade540fc62bb
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2026-02-17 09:44:26 +0530

    l

--- Basic.py
+++ Basic.py
@@ -5,7 +5,7 @@ print("This is fun.")
 print("Yay! Printing.")
 print("I'd much rather you 'not'.")
 print("I 'said' do not touch this.")
-
+# print("I 'said' do not touch this.")
 print("I like typing this.")
 print("This is fun.")
 print("Yay! Printing.")
